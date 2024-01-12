ea23ee20f740a4ad48dfdc6e5785c51264f157ad
Harun Sheikhali <harun.sheikhali@agilebits.com>
2024-01-12 18:19:31 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("#btn");
+
+
+
+button.addEventListener("click", function(evt) {
+	evt.preventDefault();
+
+	/// logic for my click
+	console.log("clicking");
+});
